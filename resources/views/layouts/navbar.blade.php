46ede50a93816a03696c0793473ddcd90ca4d118
shayanahmad1999 <shayanahmad235@gmail.com>
2024-02-13 16:04:29 +0500

    remove index.html to route

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,12 +3,12 @@
     <nav id="topbar">
         <ul class="navbar-nav theme-brand flex-row  text-center">
             <li class="nav-item theme-logo">
-                <a href="index.html">
+                <a href="{{route('admin.dashboard')}}">
                     <img src="assets/img/90x90.jpg" class="navbar-logo" alt="logo">
                 </a>
             </li>
             <li class="nav-item theme-text">
-                <a href="index.html" class="nav-link"> CORK </a>
+                <a href="{{route('admin.dashboard')}}" class="nav-link"> CORK </a>
             </li>
         </ul>
 
